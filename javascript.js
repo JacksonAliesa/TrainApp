c5f1cd312a2bb0c588818c2c880dccda2154d0aa
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-10-30 00:18:53 -0400

    made interface--input from user working correctly..need to continue with adding the new rows onto screen

---
+++ javascript.js
@@ -0,0 +1,63 @@
+// Pseudo Code
+// rows of data that are being kept within the database-firebase
+//takes in 4 inputs for train info
+//each new input gets stored in the table
+// submit --> ref push __> firebase __> onchildappend---> data display
+//submit the train request should trigger an alert that it was submitted successfully
+//calculate when the next train should arrive should be relevant to current time
+
+//initialize firebase
+// 1 hard code form that takes in name, destination, 1st train time (military format), and frequency in minutes
+// 2 hard code in HTML and empty table w/ table head of name, dest, freq, next arrival, and mins away. include a table body
+//tbody tag-- totally empty
+// 3 build on click function for submit button
+// 4 inside onclick funtion -make 4 var to capture the values form the 4 input boxes using jquery -console log var-- use ref.push firebase funciton to end
+//those variables to database
+// check firecase to see that theyre
+//5 once verified that database is working use onchild added to listen for new database values(outside of onclick function)
+
+//firebase 
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB3Qy_hcniWvFEJC4gt1YreYNmgDK0tVpo",
+    authDomain: "aliesa-s-app.firebaseapp.com",
+    databaseURL: "https://aliesa-s-app.firebaseio.com",
+    projectId: "aliesa-s-app",
+    storageBucket: "aliesa-s-app.appspot.com",
+    messagingSenderId: "147522220540",
+    appId: "1:147522220540:web:7f156b9c1e2a1fabd0c279"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //make variable to reference the database 
+
+  var database = firebase.database();
+
+  //create variables to use within the Onclick function to capture the values from the HTML file once user clicks submit on form
+  var trainName; 
+  var trainDestination; 
+  var trainTime; 
+  var trainFreq; 
+
+  //onclick function for the submit button will capture results from the user's input
+  $("button").on("click", function(event){
+   
+
+  event.preventDefault();
+  
+  trainName = $("#inputTrain").val();
+  console.log(trainName);
+  trainDestination = $("#inputDes").val();
+  console.log(trainDestination);
+  trainTime = $("#inputTime").val();
+  console.log(trainTime);
+  trainFreq = $("#inputFreq").val();
+  console.log(trainFreq);
+
+
+
+  });
+
