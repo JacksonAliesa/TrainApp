efcd8a5736cabe6a4da44ff87a5112f718cff0f6
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-10-30 18:16:35 -0400

    got the math functions running correctly for next arrival and mins away

--- javascript.js
+++ javascript.js
@@ -16,108 +16,125 @@
 // check firecase to see that theyre
 //5 once verified that database is working use onchild added to listen for new database values(outside of onclick function)
 
-//firebase 
+//firebase
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: 'AIzaSyB3Qy_hcniWvFEJC4gt1YreYNmgDK0tVpo',
+	authDomain: 'aliesa-s-app.firebaseapp.com',
+	databaseURL: 'https://aliesa-s-app.firebaseio.com',
+	projectId: 'aliesa-s-app',
+	storageBucket: 'aliesa-s-app.appspot.com',
+	messagingSenderId: '147522220540',
+	appId: '1:147522220540:web:7f156b9c1e2a1fabd0c279'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyB3Qy_hcniWvFEJC4gt1YreYNmgDK0tVpo",
-    authDomain: "aliesa-s-app.firebaseapp.com",
-    databaseURL: "https://aliesa-s-app.firebaseio.com",
-    projectId: "aliesa-s-app",
-    storageBucket: "aliesa-s-app.appspot.com",
-    messagingSenderId: "147522220540",
-    appId: "1:147522220540:web:7f156b9c1e2a1fabd0c279"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+//make variable to reference the database
 
-  //make variable to reference the database 
+var database = firebase.database();
 
-  var database = firebase.database();
+//create variables to use within the Onclick function to capture the values from the HTML file once user clicks submit on form
+var trainName;
+var trainDestination;
+var trainTime;
+var trainFreq;
 
-  //create variables to use within the Onclick function to capture the values from the HTML file once user clicks submit on form
-  var trainName; 
-  var trainDestination; 
-  var trainTime; 
-  var trainFreq; 
+//onclick function for the submit button will capture results from the user's input
+$('button').on('click', function(event) {
+	event.preventDefault();
 
-  //onclick function for the submit button will capture results from the user's input
-  $("button").on("click", function(event){
-   
+	trainName = $('#inputTrain').val();
+	console.log(trainName);
+	trainDestination = $('#inputDes').val();
+	console.log(trainDestination);
+	trainTime = $('#inputTime').val();
+	console.log(trainTime);
+	trainFreq = $('#inputFreq').val();
+	console.log(trainFreq);
 
-  event.preventDefault();
-  
-  trainName = $("#inputTrain").val();
-  console.log(trainName);
-  trainDestination = $("#inputDes").val();
-  console.log(trainDestination);
-  trainTime = $("#inputTime").val();
-  console.log(trainTime);
-  trainFreq = $("#inputFreq").val();
-  console.log(trainFreq);
-
-// adding function to input data into database that pushes each item into a new row on interface
-// this needs to be within the button click function so it can be pushed after the user CLICKS submit
-database.ref().push({
-  trainName: trainName,
-  trainDestination: trainDestination,
-  trainTime: trainTime,
-  trainFreq: trainFreq
+	// adding function to input data into database that pushes each item into a new row on interface
+	// this needs to be within the button click function so it can be pushed after the user CLICKS submit
+	database.ref().push({
+		trainName: trainName,
+		trainDestination: trainDestination,
+		trainTime: trainTime,
+		trainFreq: trainFreq
+	});
 });
 
-});
+database.ref().on('child_added', function(snapshot) {
+	var sv = snapshot.val();
 
-database.ref().on("child_added", function(snapshot){
-  var sv = snapshot.val();
+	console.log(sv.trainName);
+	console.log(sv.trainDestination);
+	console.log(sv.trainTime);
+	console.log(sv.trainFreq);
 
-  console.log(sv.trainName)
-  console.log(sv.trainDestination)
-  console.log(sv.trainTime)
-  console.log(sv.trainFreq)
+	//make a new row variable
+	var newRow = $('<tr>');
+	//make multiple TD rows var for train name, des, time, freq
+	var nameTD = $('<td>');
+	var desTD = $('<td>');
+	var freqTD = $('<td>');
+	var arrivalTD = $('<td>');
+	var minsTD = $('<td>');
 
-  //make a new row variable
-  var newRow = $("<tr>")
-  //make multiple TD rows var for train name, des, time, freq
-  var nameTD = $("<td>")
-  var desTD = $("<td>")
-  var timeTD = $("<td>")
-  var freqTD = $("<td>")
-  var minsTD = $("<td>")
+	//make text visible using the sv variable
+	nameTD.text(sv.trainName);
+	//append to the new row
+	newRow.append(nameTD);
 
-  //make text visible using the sv variable 
-   nameTD.text(sv.trainName);
-   //append to the new row
-   newRow.append(nameTD);
-   
-   //make text visible
-   desTD.text(sv.trainDestination);
-   //append to the new row
-   newRow.append(desTD);
+	//make text visible
+	desTD.text(sv.trainDestination);
+	//append to the new row
+	newRow.append(desTD);
 
-    //make text visible
-    freqTD.text(sv.trainFreq);
-    //append to the new row
-    newRow.append(freqTD);
+	//make text visible
+	freqTD.text(sv.trainFreq);
+	//append to the new row
+	newRow.append(freqTD);
 
-   //make text visible
-   timeTD.text();
-   //append to the new row
-   newRow.append(timeTD);
+	//set the freqTD variable to 5mins made new variable for the first train time of 10:00
+	// use svtrainfreq to use in calculations to get the train freq input
+	var firstTime = sv.trainTime;
+	var freq = sv.trainFreq;
+	//next arrival conversion
 
+	// First Time (pushed back 1 year to make sure it comes before current time)
+	var firstTimeConverted = moment(firstTime, 'HH:mm').subtract(1, 'years');
+	console.log(firstTimeConverted);
+
+
+	// Difference between the times
+	var diffTime = moment().diff(moment(firstTimeConverted), 'minutes');
+	console.log('DIFFERENCE IN TIME: ' + diffTime);
+
+	// Time apart (remainder)
+	var tRemainder = diffTime % freq;
+	console.log(tRemainder);
+
+	// Minute Until Train
+	var tMinutesTillTrain = freq - tRemainder;
+	console.log('MINUTES TILL TRAIN: ' + tMinutesTillTrain);
+
+	// Next Train
+	var nextTrain = moment().add(tMinutesTillTrain, 'minutes');
+	console.log('ARRIVAL TIME: ' + moment(nextTrain).format('hh:mm'));
+
+	//variable for the next arrival to append to row
+	arrivalTD.text(moment(nextTrain).format('hh:mm'));
+	//append next arrival TD to new row
+  newRow.append(arrivalTD);
   
-
-  //make text visible 
-  minsTD.text()
+ 
+  //variable for mins away TD
+  minsTD.text(tMinutesTillTrain);
   //append to new row
   newRow.append(minsTD);
 
 
-  //append new row variable to tbody
-  $("tbody").append(newRow)
-
-
+	//append new row variable to tbody
+	$('tbody').append(newRow);
 });
-
-
-
